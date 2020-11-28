3209fba9b01c6b85c6ffedc953c28f8dac146e59
abdi mussa <abdimussa87@gmail.com>
2020-11-28 12:02:06 +0300

    Fixed bug that allowed admin to sign in using normal users signin route

--- src/controller/authController.js
+++ src/controller/authController.js
@@ -27,7 +27,7 @@ export const signin = (req, res) => {
         if (err) {
             res.status(500).json({ message: err })
         } else if (user) {
-            if (user.authenticate(req.body.password)) {
+            if (user.authenticate(req.body.password) && user.role === 'user') {
                 const token = jwt.sign({ _id: user._id, role: user.role }, process.env.JWT_SECRET, { expiresIn: '1h' });
                 const { _id, firstName, lastName, email, role, fullName } = user;
                 res.status(200).send({ token, user: { _id, firstName, lastName, email, role, fullName } })
